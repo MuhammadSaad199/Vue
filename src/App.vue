0ba157efa0b5ee9cc59a681c604e429b7025433d
muhammadsaad199 <mohammadsaadjavediqbal999@gmail.com>
2024-03-18 17:01:35 +0500

    add vuejs components

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import {ref} from 'vue'
 // import dynamicComponent from './components/dynamicComponent.vue';
 import propValidation from './components/propValidation.vue';
 
-const firstname = ref('Muhammad');
+// const firstname = ref('Muhammad');
 // const lastName = ref('Ali');
 
-const age = ref(34)
+// const age = ref(34)
 
 </script>
 
@@ -16,10 +16,15 @@ const age = ref(34)
 <!-- <HelloWorld /> -->
 <!-- <staticComponent    name="firstName"/>
 <dynamicComponent :first-name="firstName" :last-name="lastName"/> -->
-<propValidation  
+
+
+<!-- <propValidation  
     :friend="['ali', 'ahmed' , 'asghar', 'raza' , 'asad','aman']"
     :userInfo="{name:'ali', age:'39', earth:['earth','idk']}"
-/>
+/> -->
+
+
+<propValidation name="ali"  age="22" password="useme"  />
 </template>
 
 <style scoped>
--- src/components/propValidation.vue
+++ src/components/propValidation.vue
@@ -13,18 +13,39 @@
 //     age: Number
 // })
 
-defineProps({
-    friend: Array,
-    userInfo: Object,
+// defineProps({
+//     friend: Array,
+//     userInfo: Object,
+// })
+
+defineProps ({
+    name: {
+        type: String,
+        validator: (propValue) => {
+            return ["ali","sirat", "sawat"].includes(propValue)
+        }
+    },
+    age: {
+            type: Number,
+            validator: (propValue) => (propValue >= 18 && propValue <= 30 ? true : false)
+        },
+    password: {
+        type: String,
+        validator: (propValue) => propValue.length >= 8 
+    }
 })
 </script>
 
 <template>
     <!-- <p>{{ firstname }}</p>
     <p>{{ age }}</p> -->
-<ul>
-<li v-for="(friends, index) in friend" :key="index">{{ friends }}</li></ul>
-<ul><li v-for="(info, i) in userInfo" :key="i">{{ info }}</li></ul>
 
+<!-- <ul><li v-for="(friends, index) in friend" :key="index">{{ friends }}</li></ul>
+<ul><li v-for="(info, i) in userInfo" :key="i">{{ info }}</li></ul> -->
+
+
+<p>Name: {{ name }}</p>
+<p>Age: {{ age }}</p>
+<p>Password: {{ password }}</p>
 
 </template>
