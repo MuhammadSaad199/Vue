e030f9d815d7dbea78e1352aed8b4bbbbdd8fe48
muhammadsaad199 <mohammadsaadjavediqbal999@gmail.com>
2024-03-19 17:27:32 +0500

    compo events

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@ import {ref} from 'vue'
 // import HelloWorld from './components/HelloWorld.vue'
 // import staticComponent from './components/staticComponent.vue';
 // import dynamicComponent from './components/dynamicComponent.vue';
-import propValidation from './components/propValidation.vue';
+// import propValidation from './components/propValidation.vue';
+// import childComponent from './components/childComponent.vue';
+import slot from './components/slot.vue';
 
 // const firstname = ref('Muhammad');
 // const lastName = ref('Ali');
 
 // const age = ref(34)
+
+// const formHandler = (name, email, password)=>{
+//     console.log("UserName",name)
+//     console.log("Email",email)
+//     console.log("Password",password)
+// }
 
 </script>
 
@@ -24,7 +32,12 @@ import propValidation from './components/propValidation.vue';
 /> -->
 
 
-<propValidation name="ali"  age="22" password="useme"  />
+<!-- <propValidation name="ali"  age="22" password="useme"  /> -->
+
+<!-- <childComponent @userInfo="formHandler"/> -->
+
+
+
 </template>
 
 <style scoped>
---
+++ src/components/childComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref("")
+const email = ref("")
+const password = ref("")
+
+</script>
+
+<template>
+    <form @submit.prevent="$emit('userInfo', name, email, password)">
+        <input type="text" placeholder="Name" v-model="name">
+        <input type="email" placeholder="email" v-model="email">
+        <input type="password" placeholder="password" v-model="password">
+        <button :disabled="password.length<8" type="submit">Submit</button>
+    </form>
+</template>
